f45782b8637c7a25f1a322a2b40dc7db9c743e19
Aadam Zocolo <azocolo@gmail.com>
2021-01-06 20:42:51 -0500

    Changes to DB interface

--- src/hddmondtools/databaseinterface.py
+++ src/hddmondtools/databaseinterface.py
@@ -1,13 +1,16 @@
-from .hddmon_dataclasses import HddData, TaskData, AttributeData, ImageData
+from .hddmon_dataclasses import HddData, TaskData
 from abc import ABC, abstractmethod
 
-class GenericDatabase(ABC):
+class DatabaseInterface(ABC):
     '''
     This class defines the structure needed for a database implimentation on hddmond.
     '''
 
+    def __init__(self):
+        pass
+
     @abstractmethod
-    def connect(self, *a, **kw):
+    async def connect(self, *a, **kw):
         '''
         When overridden, should attempt to connect to a database with supplied info in __init__.
         Returns weather connecting succeeded or not.
@@ -18,28 +21,58 @@ class GenericDatabase(ABC):
         return False
     
     @abstractmethod
-    def disconnect(self):
+    async def disconnect(self):
         '''
         When overridden, should disconnect (if applicable) from the database.
         '''
         pass
 
     @abstractmethod
-    def update_hdd(self, hdd: HddData):
+    async def connected(self):
+        '''
+        This method should return if the database is currently connected.
+        '''
         pass
 
     @abstractmethod
-    def see_hdd(self, serial: str):
+    async def update_hdd(self, hdd: HddData):
+        '''
+        This method updates the HDD's information with the latest and greatest data. 
+        
+        For example, when a HDD we've seen before is inserted, let's update the information
+        just in case any of it has changed.
+        '''
         pass
 
     @abstractmethod
-    def add_task(self, serial: str, task: TaskData):
+    async def see_hdd(self, serial: str) -> int:
+        '''
+        This method will "see" a HDD, which increases the 'seen' counter in the database. 
+        This method should return the new 'seen' value.
+        '''
         pass
 
-    # @abstractmethod
-    # def decommission(self, decommissioned=True):
-    #     pass
+    @abstractmethod
+    async def add_task(self, serial: str, task: TaskData):
+        '''
+        This method is called when a task is completed (successfully or not) on a HDD. 
+        The task's data should be saved to be referenced later for historical data.
+        '''
+        pass
 
     @abstractmethod
-    def insert_attribute_capture(self, hdd: HddData):
+    async def decommission(self, decommissioned=True):
+        '''
+        This method will decomission a HDD should the user decide to do that. 
+        A decommissioned HDD will have a different look on the UI and alert the user
+        that the HDD is decommissioned.
+        '''
+        pass
+
+    @abstractmethod
+    async def insert_attribute_capture(self, hdd: HddData):
+        '''
+        This method will insert a capture of the HDD's SMART data. This information is useful for
+        historical attribute trends. 
+        '''
         pass
